fe50121406a87bc4de327d9d06ee7640cf00eb0c
hoon2 <sanghoon.engineer@gmail.com>
2022-10-15 15:29:48 +0900

    usetoggle 추가

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 function reducer(state, action) {
   switch (action.type) {
     case "INCREMENT":
@@ -6,20 +6,33 @@ function reducer(state, action) {
     case "DECREMENT":
       return state - 1;
     case "INCREMENT_DOUBLE":
-      return state + action.payload
+      return state + action.payload;
     default:
       throw new Error("Unhandled action");
   }
 }
 function Counter() {
   const [number, dispatch] = useReducer(reducer, 0);
+  const [state, toggleState] = useReducer((state) => !state, true);
+
+
   return (
     <>
       <h1>숫자:{number}</h1>
+      <button
+        style={{ background: state ? "red" : "black" }}
+        onClick={() => toggleState()}
+      >
+        토글 버튼
+      </button>
       <div>
         <button onClick={() => dispatch({ type: "INCREMENT" })}>+1</button>
         <button onClick={() => dispatch({ type: "DECREMENT" })}>-1</button>
-        <button onClick={() => dispatch({ type: "INCREMENT_DOUBLE", payload: 2 })}>+2</button>
+        <button
+          onClick={() => dispatch({ type: "INCREMENT_DOUBLE", payload: 2 })}
+        >
+          +2
+        </button>
       </div>
     </>
   );
