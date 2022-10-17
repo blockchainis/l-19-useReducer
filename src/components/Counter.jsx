a1a4427f2d86c354203eb10cf7c01594d3edee7f
hoon2 <sanghoon.engineer@gmail.com>
2022-10-17 12:44:43 +0900

    useReducer초기코드

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,38 +1,13 @@
-import { useReducer, useState } from "react";
-function reducer(state, action) {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    case "INCREMENT_DOUBLE":
-      return state + action.payload;
-    default:
-      throw new Error("Unhandled action");
-  }
-}
+import { useState } from "react";
+
 function Counter() {
-  const [number, dispatch] = useReducer(reducer, 0);
-  const [state, toggleState] = useReducer((state) => !state, true);
-
-
+  const [number, setNumber] = useState(0);
   return (
     <>
       <h1>숫자:{number}</h1>
-      <button
-        style={{ background: state ? "red" : "black" }}
-        onClick={() => toggleState()}
-      >
-        토글 버튼
-      </button>
       <div>
-        <button onClick={() => dispatch({ type: "INCREMENT" })}>+1</button>
-        <button onClick={() => dispatch({ type: "DECREMENT" })}>-1</button>
-        <button
-          onClick={() => dispatch({ type: "INCREMENT_DOUBLE", payload: 2 })}
-        >
-          +2
-        </button>
+        <button>+1</button>
+        <button>-1</button>
       </div>
     </>
   );
